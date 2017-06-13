d038b3f7842d638afbc4fea31d7458c29229b647
Your Name <you@example.com>
2017-06-13 19:03:44 -0400

    api updates, server working, trip api done

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -8,30 +8,39 @@ module.exports = function(app) {
 
   // index route loads view.html
   app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/blog.html"));
+    res.sendFile(path.join(__dirname, "../public/home.html"));
   });
 
   // cms route loads cms.html
-  app.get("/TripOverview", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/cms.html"));
+  app.get("/tripOverview", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/tripOverview.html"));
   });
 
   // blog route loads blog.html
-  app.get("/AddMember", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/blog.html"));
+  app.get("/addMember", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/addMember.html"));
   });
 
-  app.get("/Transportation", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/author-manager.html"));
+  app.get("/transportation", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/transportation.html"));
   });
 
-  app.get("/GetTrip", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/author-manager.html"));
+  app.get("/createTrip", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/createTrip.html"));
   });
 
-  app.get("/Necessities", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/author-manager.html"));
+  app.get("/necessities", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/necessities.html"));
   });
+
+  app.get("/createItemList", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/createItemList.html"));
+  });
+
+  app.get("/selectItems", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/selectItems.html"));
+  });
+
 
 
 };
