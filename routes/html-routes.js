cd5fcf9fb9e6579884d915dac20b807716eb8b32
Marie Gadda <mariegadda@gmail.com>
2017-06-15 20:03:56 -0400

    added handlebars

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,45 +1,64 @@
 var path = require("path");
-
+//imports the model, burger.js to use its database functions
+var db = require("../models");
+var express = require("express");
+var router = express.Router();
 // Routes
 // =============================================================
 module.exports = function(app) {
 
-  // Each of the below routes just handles the HTML page that the user gets sent to.
+// Each of the below routes just handles the HTML page that the user gets sent to.
 
-  // index route loads view.html
-  app.get("/", function(req, res) {
+// index route loads view.html
+app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/home.html"));
-  });
+});
 
-  // cms route loads cms.html
-  app.get("/tripOverview", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/tripOverview.html"));
-  });
+//handlebars route for tripOverview page - WORKS
+app.get("/tripOverview", function(req, res) {
+    db.Trip.findAll({})
+        .then(function(tripName) {
+            var hbsObject = { trip: tripName };
+            res.render("../views/tripOverview", hbsObject);
+        });
+});
 
-  // blog route loads blog.html
-  app.get("/addMember", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/addMember.html"));
-  });
+// handlebars route for addMember page
+app.get("/addMember", function(req, res){
+  db.Member.findAll({})
+    .then(function(memberName) {
+      var hbsObject = {member: memberName};
+      res.render("../views/addMember", hbsObject);
+    });
+});
 
-  app.get("/transportation", function(req, res) {
+app.get("/transporation", function(req, res){
+  db.Member.findAll({})
+    .then(function(memberCar) {
+      var hbsObject = {member: memberCar};
+      res.render("../views/transportation", hbsObject);
+    });
+});
+
+app.get("/transportation", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/transportation.html"));
-  });
+});
 
-  app.get("/createTrip", function(req, res) {
+app.get("/createTrip", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/createTrip.html"));
-  });
+});
 
-  app.get("/necessities", function(req, res) {
+app.get("/necessities", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/necessities.html"));
-  });
+});
 
-  app.get("/createItemList", function(req, res) {
+app.get("/createItemList", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/createItemList.html"));
-  });
+});
 
-  app.get("/selectItems", function(req, res) {
+app.get("/selectItems", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/selectItems.html"));
-  });
+});
 
 
 
